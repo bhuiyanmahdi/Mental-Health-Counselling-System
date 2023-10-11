d3cf98ac50b0ed2ff1e9c0b001d3a8d3466857d1
mahjabinsultana <mahjabinsultana7020@gmail.com>
2023-10-11 09:19:29 +0600

    chatbot activity

---
+++ app/src/main/java/com/example/mentalhealthcounselling1/ChatbotActivity.java
@@ -0,0 +1,86 @@
+package com.example.mentalhealthcounselling1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ChatbotActivity extends AppCompatActivity {
+
+    private RecyclerView chatsRV;
+    private EditText userMsgEdt;
+    private FloatingActionButton sendMsgFAB;
+    private final String BOT_KEY = "bot";
+    private final String USER_KEY = "user";
+    private ArrayList<ChatsModal> chatsModalArrayList;
+    private ChatRVAdapter chatRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+        chatsRV = findViewById(R.id.idRVChats);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+        sendMsgFAB = findViewById(R.id.idFABSend);
+        chatsModalArrayList = new ArrayList<>();
+
+        chatRVAdapter = new ChatRVAdapter(chatsModalArrayList, this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        chatsRV.setLayoutManager(manager);
+        chatsRV.setAdapter(chatRVAdapter);
+
+        sendMsgFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userMsgEdt.getText().toString().isEmpty()){
+                    Toast.makeText(ChatbotActivity.this, "Please enter your message", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                getResponse(userMsgEdt.getText().toString());
+                userMsgEdt.setText("");
+
+            }
+        });
+    }
+    private void getResponse(String message){
+        chatsModalArrayList.add(new ChatsModal(message, USER_KEY));
+        chatRVAdapter.notifyDataSetChanged();
+        String url="http://127.0.0.1:5000/chat";
+        String BASE_URL = "http://127.0.0.1:5000";
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<MsgModal> call = retrofitAPI.getMessage(url);
+        call.enqueue(new Callback<MsgModal>() {
+            @Override
+            public void onResponse(Call<MsgModal> call, Response<MsgModal> response) {
+                if(response.isSuccessful())
+                {
+                    MsgModal modal = response.body();
+                    chatsModalArrayList.add(new ChatsModal(modal.getChatBotReply(), BOT_KEY));
+                    chatRVAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MsgModal> call, Throwable t) {
+                chatsModalArrayList.add(new ChatsModal("Please revert your question", BOT_KEY));
+                chatRVAdapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mentalhealthcounselling1/MsgModal.java
@@ -0,0 +1,17 @@
+package com.example.mentalhealthcounselling1;
+
+public class MsgModal {
+    private String chatBotReply;
+
+    public MsgModal(String chatBotReply) {
+        this.chatBotReply = chatBotReply;
+    }
+
+    public String getChatBotReply() {
+        return chatBotReply;
+    }
+
+    public void setChatBotReply(String chatBotReply) {
+        this.chatBotReply = chatBotReply;
+    }
+}
