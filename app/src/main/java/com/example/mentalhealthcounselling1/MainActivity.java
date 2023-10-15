d120642e3d37e226c280f469a99c82b979f43e74
mahjabinsultana <mahjabinsultana7020@gmail.com>
2023-10-15 11:20:55 +0600

    chatbot update 3

--- app/src/main/java/com/example/mentalhealthcounselling1/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthcounselling1/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.mentalhealthcounselling1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.idChatbot);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, ChatbotActivity.class);
+        startActivity(intent);
+
     }
 
 }
